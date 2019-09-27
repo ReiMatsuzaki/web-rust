bfb7b9e8e131b9338459ffcf294e0ee46769506a
Matsuzaki Rei <matsuzaki.rei@gmail.com>
2019-09-27 21:38:17 +0900

    separate http response

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::net::{TcpListener, TcpStream};
 use std::thread;
-use std::io::{BufRead, BufReader, copy, Write, Read};
+use std::io::{BufRead, BufReader, Write, Read};
 use std::path::PathBuf;
 use std::fs::File;
 use std::collections::HashMap;
@@ -20,8 +20,8 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn handle_client(stream: TcpStream) {
-    let mut stream = BufReader::new(stream);
+fn handle_client(tcp_stream: TcpStream) {
+    let mut stream: BufReader<TcpStream> = BufReader::new(tcp_stream);
 
     let mut first_line = String::new();
     if let Err(err) = stream.read_line(&mut first_line) {
@@ -31,32 +31,33 @@ fn handle_client(stream: TcpStream) {
     let mut params = first_line.split_whitespace();
     let method = params.next();
     let path = params.next();
-    match (method, path) {
+    let res = match (method, path) {
         (Some("GET"), Some(path)) => {
-            get_operation(path, stream.get_mut());
+            get_operation(path)
         }
-        _ => panic!("failed to parse"),
-    }
+        _ => http_response_error()
+    };
+
+    let mut tcp_stream = stream.into_inner();
+    write_http_response(&res, &mut tcp_stream);
 }
 
-fn get_operation(path: &str, stream: &mut TcpStream) {
+fn get_operation(path: &str) -> HttpResponse {
     let path = PathBuf::from(format!("/Users/matsuzakirei/src/github.com/ReiMatsuzaki/web-rust/www{}", path));
     let open_file = File::open(&path);
     match open_file {
         Ok(file) => {
-            let len = file.metadata().map(|m| m.len()).unwrap_or(0);
-
-            writeln!(stream, "HTTP/1.1 200 OK").unwrap();
-            writeln!(stream, "Content-Type: text/html; charset=UTF-8").unwrap();
-            writeln!(stream, "Content-Length: {}", len).unwrap();
-            writeln!(stream).unwrap();
+            let mut body = String::new();
             let mut file2 = file; // TODO: this code seems illegal
-            copy(&mut file2, stream).unwrap();
+            match file2.read_to_string(&mut body) {
+                Ok(_) => http_response_ok(body),
+                Err(_) => http_response_error()
+            }
         },
         Err(_) => {
-            writeln!(stream, "HTTP/1.1 501 ERR").unwrap();
+            http_response_error()
         },
-    };
+    }
 }
 
 struct HttpResponse {
@@ -74,15 +75,12 @@ fn write_http_response(res: &HttpResponse, stream: &mut TcpStream) {
     let line = format!("HTTP/{} {} {}", res.version, res.code, msg);
     writeln!(stream, "{}", &line).unwrap();
     for (k, v) in &res.key_values {
-        writeln!(stream, "{}: {}", k, v);
+        writeln!(stream, "{}: {}", k, v).unwrap();
     }
-    writeln!(stream);
-    writeln!(stream, "{}", res.body);
+    writeln!(stream).unwrap();
+    writeln!(stream, "{}", res.body).unwrap();
 }
-fn http_response_ok(file: &mut File) -> HttpResponse {
-
-    let mut body = String::new();
-    file.read_to_string(&mut body);
+fn http_response_ok(body: String) -> HttpResponse {
 
     let len = format!("{}", body.len().to_string());
 
@@ -95,7 +93,16 @@ fn http_response_ok(file: &mut File) -> HttpResponse {
         code: 200,
         key_values: key_values,
         body};
-
+}
+fn http_response_error() -> HttpResponse {
+    let key_values = HashMap::new();
+    let body = String::new();
+    HttpResponse{
+        version: "1.1".to_string(),
+        code: 501,
+        key_values: key_values,
+        body
+    }
 }
 
 
